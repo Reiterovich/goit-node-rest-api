bedf5103fcc152342d549fa08b1d5c894a7302f3
Reiterovich <reiterovich.andriy@gmail.com>
2024-03-09 12:08:36 +0200

    3.1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,11 +2,6 @@ import * as contactsService from "../services/contactsServices.js";
 
 import HttpError from "../helpers/HttpError.js";
 
-// import {
-//   createContactSchema,
-//   updateContactSchema,
-// } from "../schemas/contactsSchemas.js";
-
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
 const getAllContacts = async (req, res) => {
@@ -25,19 +20,11 @@ const getOneContact = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
-  // const { error } = createContactSchema.validate(req.body);
-  // if (error) {
-  //   throw HttpError(400, error.message);
-  // }
   const result = await contactsService.addContact(req.body);
   res.status(201).json(result);
 };
 
 const updateContact = async (req, res) => {
-  // const { error } = updateContactSchema.validate(req.body);
-  // if (error) {
-  //   throw HttpError(400, error.message);
-  // }
   const { id } = req.params;
   const result = await contactsService.updateContactById(id, req.body);
   if (!result) {
